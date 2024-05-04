b29a3aca0e07389581623dac6591ad42d4659160
Arabambi Oluwaseun <161367500+oluwaseunarabambi@users.noreply.github.com>
2024-05-04 07:08:48 -0300

    Add files via upload

---
+++ land.js
@@ -0,0 +1,47 @@
+let A = `Machala `;
+let B = `MMC `;
+
+console.log(A+B);
+
+
+
+let Z = `Machala`;
+
+console.log(`${Z} Welcome to Sail`);
+
+
+let Y = `MMC`;
+
+console.log(Y.toUpperCase(Y));
+
+let P = `God Bless You`;
+let G = `I Love Sail`;
+let S = `Sail Innovation `;
+
+console.log(S.padStart(S));
+
+//numbers data types in JS//
+
+const c = `MMC`;
+
+console.log(c.charAt(`b:2`));
+
+console.log(c.lastIndexOf(`c`));
+
+const a = 30;
+const b = 40;
+const d = 50;
+const e = 60;
+
+const f = a+b+d;
+const i = e-a+4;
+const j = e/1;
+const k = Math.floor(6);  
+const l = Math.round(10);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+
+const m = k+l;
+
+console.log(`Machala: ${m}`);
+console.log(k);
+console.log(j.valueOf(10));
+
